4847f82243b641639475cc533c34c39f0b449109
mfary <moha1379>
2019-04-15 15:02:07 +0430

    build abstract class card and put its field and getters and constructor and put it in its package

---
+++ src/cards/Card.java
@@ -0,0 +1,31 @@
+package cards;
+
+public abstract class Card{
+    protected String name;
+    protected String id;
+    protected String description;
+    protected int price;
+
+    public Card(String name , String id , String description , int price){
+        this.name = name;
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
