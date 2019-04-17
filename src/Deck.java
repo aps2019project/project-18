4fdfc995885f79494a0e3978cee5a8c5b519593b
AliLordLoss <a.a.ghanati@gmail.com>
2019-04-17 18:02:34 +0430

    added findCard, addCard and removeCard methods to class Deck

--- src/Deck.java
+++ src/Deck.java
@@ -11,5 +11,37 @@ public class Deck {
         this.name = name;
     }
 
+    public Card findCard(String cardId) {
+        for (Card card : cards) {
+            if (card.getId().compareTo(cardId) == 0) {
+                return card;
+            }
+        }
+        return null;
+    }
 
+    public void addCard(Card card) {
+        if (cards.size() < 20 && findCard(card.getId()) == null) {
+            cards.add(card);
+            System.out.println("card added");
+            return;
+        } else if (cards.size() == 20) {
+            System.out.println("deck is full");
+            return;
+        } else {
+            System.out.println("card already in deck");
+            return;
+        }
+    }
+
+    public void removeCard(Card card) {
+        if (findCard(card.getId()) == null) {
+            System.out.println("card isn't in deck");
+            return;
+        } else {
+            cards.remove(card);
+            System.out.println("card removed from deck");
+            return;
+        }
+    }
 }
