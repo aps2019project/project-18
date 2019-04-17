209d554ee453f89a43a27714e7d2bf3724e8bd51
AliLordLoss <a.a.ghanati@gmail.com>
2019-04-17 17:39:41 +0430

    made class Spell and with its fields and functions

---
+++ src/cards/Spell.java
@@ -0,0 +1,18 @@
+package cards;
+
+public class Spell extends Card{
+    private int manaPoint;
+    private String target;
+
+    public int getMP() {
+        return manaPoint;
+    }
+
+    public void executeBuff(Playground playground, int x, int y) {
+        // TODO
+    }
+
+    public void execute() {
+        // TODO
+    }
+}
