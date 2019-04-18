ee33cf60f6b6d2a7aa98d33628d67beab7310bba
mfary <moha1379>
2019-04-19 02:01:05 +0430

    create stunBuff class

---
+++ src/Modules/PlayableThings/BuffAndSpecialPowers/Buff/StunBuff.java
@@ -0,0 +1,14 @@
+package Modules.PlayableThings.BuffAndSpecialPowers.Buff;
+
+public class StunBuff extends  Buff {
+    private boolean positivity = false;
+
+    public boolean isPositivity() {
+        return positivity;
+    }
+
+    @Override
+    public void doEffect() {
+        super.doEffect();
+    }
+}
