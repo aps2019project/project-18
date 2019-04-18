f41f8e12f37b870c231933ff12420903b8cb97c0
mfary <moha1379>
2019-04-19 01:22:02 +0430

    create hero class

---
+++ src/Modules/cards/Hero.java
@@ -0,0 +1,19 @@
+package Modules.cards;
+
+public class Hero extends Force{
+    private  int spellCoolDown;
+    private int usedSpellCoolDown = 0;
+    private Spell spell;
+
+    public void excuteAbility(){
+        if (usedSpellCoolDown == 0){
+            usedSpellCoolDown = spellCoolDown;
+            //todo
+        }
+    }
+
+    public void decreaseCoolDown(){
+        if (usedSpellCoolDown > 0)
+            usedSpellCoolDown--;
+    }
+}
