c030ad804e1a390bc51e474c6040f73a95e73323
mfary <moha1379>
2019-04-19 01:44:23 +0430

    create HollyBuff class

---
+++ src/Modules/PlayableThings/BuffAndSpecialPowers/Buff/HollyBuff.java
@@ -0,0 +1,14 @@
+package Modules.PlayableThings.BuffAndSpecialPowers.Buff;
+
+public class HollyBuff extends Buff {
+    private boolean positivity = true;
+
+    @Override
+    public void doEffect() {
+        super.doEffect();
+    }
+
+    public boolean isPositivity() {
+        return positivity;
+    }
+}
