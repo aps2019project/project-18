3b7db8d54b3490c141b1bad8b2fda5e5e08567fb
mfary <moha1379>
2019-04-18 14:51:04 +0430

    complete Force

--- src/Modules/cards/Force.java
+++ src/Modules/cards/Force.java
@@ -1,5 +1,7 @@
 package Modules.cards;
 
+import java.util.ArrayList;
+
 public abstract class Force extends Card {
     protected int attackPower;
     protected int healthPower;
@@ -7,4 +9,29 @@ public abstract class Force extends Card {
     protected String attackType;
     protected boolean canMove;
     protected int range;
+    protected ArrayList<Buff> buffs;
+
+    void addBuff(Buff buff){
+        //todo
+    }
+
+    public boolean getFlag(){
+        //todo
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public int getHealthPower() {
+        return healthPower;
+    }
+
+    public void checkBuff(){
+        //todo
+    }
 }
