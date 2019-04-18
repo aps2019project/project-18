96ab2ea751e63cecce0b21a1b6fb9f12818f9d03
mfary <moha1379>
2019-04-19 01:59:52 +0430

    create poisonBuff class

---
+++ src/Modules/PlayableThings/BuffAndSpecialPowers/Buff/PoisonBuff.java
@@ -0,0 +1,14 @@
+package Modules.PlayableThings.BuffAndSpecialPowers.Buff;
+
+public class PoisonBuff extends Buff{
+    private boolean positivity = false;
+
+    public boolean isPositivity() {
+        return positivity;
+    }
+
+    @Override
+    public void doEffect() {
+        super.doEffect();
+    }
+}
