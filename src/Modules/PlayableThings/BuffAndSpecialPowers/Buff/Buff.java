e1cb106352abc3fdfac89640bc4b147f0f34e90c
mfary <moha1379>
2019-04-19 01:40:11 +0430

    create buff class and its package

---
+++ src/Modules/PlayableThings/BuffAndSpecialPowers/Buff/Buff.java
@@ -0,0 +1,12 @@
+package Modules.PlayableThings.BuffAndSpecialPowers.Buff;
+
+public class Buff {
+    protected int numberOfTurns;
+    protected int[] effectPoint = new int[2];
+    protected int dimenision;
+
+    public void doEffect(){
+        //todo
+    }
+
+}
