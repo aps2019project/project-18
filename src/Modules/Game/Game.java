9c490895df963260de13efca37c163ee46ca57f5
invoker <omidrahmati2000@gmail.com>
2019-04-19 13:16:37 +0430

    create abstract class Game and finished it some import and add one method to class Player

---
+++ src/Modules/Game/Game.java
@@ -0,0 +1,37 @@
+package Modules.Game;
+
+import Modules.GameBusiness.Player.Player;
+import Modules.GameData;
+import Modules.PlayableThings.Item.Item;
+
+import java.util.ArrayList;
+
+public abstract class Game {
+    protected Player playerOne = new Player();
+    protected Player playerTwo = new Player();
+    protected int turn;
+    protected boolean end;
+    protected int winnerPlayer;
+    private static ArrayList<Item> collectableItems = new ArrayList<>();
+
+    abstract public void turn();
+
+    public void saveData() {
+        String playerOneName = playerOne.getAccount().getUserName();
+        String playerTwoName = playerTwo.getAccount().getUserName();
+        String matchResultPlayerOne, matchResultPlayerTwo;
+        if (winnerPlayer == 1) {
+            matchResultPlayerOne = "win";
+            matchResultPlayerTwo = "lose";
+        } else {
+            matchResultPlayerOne = "lose";
+            matchResultPlayerTwo = "win";
+        }
+        playerOne.getAccount().saveGameData(new GameData(playerTwoName, matchResultPlayerOne));
+        playerTwo.getAccount().saveGameData(new GameData(playerOneName, matchResultPlayerTwo));
+    }
+
+    abstract public void setPlayground();
+
+    abstract protected void checkEnd();
+}
