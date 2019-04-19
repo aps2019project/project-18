818f6b735c3797fd9367f7734a2c2d49b15a3e6b
mfary <moha1379>
2019-04-19 14:33:38 +0430

    correct name of constants

--- src/View/Request/PlayerRequest.java
+++ src/View/Request/PlayerRequest.java
@@ -1,17 +1,17 @@
 package View.Request;
 
 public class PlayerRequest extends MainRequest{
-    private static final String GAMEINFO = "Game info";
-    private static final String SHOWMYMINIONS = "Show my minions";
-    private static final String SHOWOPPONNENTSMINIONS = "Show opponent minions";
-    private static final String SHOWCARDINFO = "Show card info";
+    private static final String GAME_INFO = "Game info";
+    private static final String SHOW_MY_MINIONS = "Show my minions";
+    private static final String SHOW_OPPONNENT_MINIONS = "Show opponent minions";
+    private static final String SHOW_CARD_INFO = "Show card info";
     private static final String SHOW = "Show";
     private static final String SELECT = "Select";
-    private static final String SHOWHAND = "Show hand";
-    private static final String INSERTCARD = "Insert";
-    private static final String ENDTURN = "End turn";
-    private static final String SHOWITEMS = "Show collectables";
-    private static final String SHOWNEXTCARD = "Show Next Card";
-    private static final String ENTERGRAVEYARD = "Enter graveyard";
+    private static final String SHOW_HAND = "Show hand";
+    private static final String INSERT_CARD = "Insert";
+    private static final String END_TURN = "End turn";
+    private static final String SHOW_ITEMS = "Show collectables";
+    private static final String SHOW_NEXT_CARD = "Show Next Card";
+    private static final String ENTER_GRAVEYARD = "Enter graveyard";
     private static final String HELP = "Help";
 }
