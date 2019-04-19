bbddf1772331b152af22bc7110ab1165b00bdf58
invoker <omidrahmati2000@gmail.com>
2019-04-19 13:30:20 +0430

    add game mode classes and change method turn in class Game

---
+++ src/Modules/Game/ModeKillEnemyHero.java
@@ -0,0 +1,14 @@
+package Modules.Game;
+
+public class ModeKillEnemyHero extends Game {
+
+    @Override
+    public void setPlayground() {
+
+    }
+
+    @Override
+    protected void checkEnd() {
+
+    }
+}
