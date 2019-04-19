483167a27787cd968b2545f4397887d5768b5921
invoker <omidrahmati2000@gmail.com>
2019-04-19 11:07:52 +0430

    finished class Playground for now

--- src/Modules/Playground.java
+++ src/Modules/Playground.java
@@ -13,11 +13,15 @@ public class Playground {
     public int[] isCardOnGround(String cardId) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 5; j++) {
-                if(houses[i][j].getCard().getId().equals(cardId)) {
+                if (houses[i][j].getCard().getId().equals(cardId)) {
                     return new int[]{i, j};
                 }
             }
         }
         return null;
+    }
+
+    public House[][] getGround() {
+        return houses;
     }
 }
