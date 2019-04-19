c3fbd45ceacfbc04c750a402dea338983e8934e0
mfary <moha1379>
2019-04-19 13:01:41 +0430

    crate human class

---
+++ src/Modules/GameBusiness/Player/Human.java
@@ -0,0 +1,12 @@
+package Modules.GameBusiness.Player;
+
+import Modules.Account;
+
+public class Human extends Player {
+    private Account account;
+
+    @Override
+    public void playTurn() {
+        //todo
+    }
+}
