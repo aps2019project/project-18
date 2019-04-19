63eb61a2e1b6b3139ded5c571063102dc76b1043
invoker <omidrahmati2000@gmail.com>
2019-04-19 11:06:08 +0430

    add method isCardOnGround to class Playground

--- src/Modules/Playground.java
+++ src/Modules/Playground.java
@@ -9,4 +9,15 @@ public class Playground {
         houses[destinationX][destinationY].setCard(houses[homeX][homeY].getCard());
         houses[homeX][homeY].removeCard();
     }
+
+    public int[] isCardOnGround(String cardId) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(houses[i][j].getCard().getId().equals(cardId)) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
 }
