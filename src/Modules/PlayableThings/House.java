af82357859e8fe212995a046bea3fda4d2b14395
invoker <omidrahmati2000@gmail.com>
2019-04-19 10:25:52 +0430

    add all method in uml to class House (class House done for now)

--- src/Modules/PlayableThings/House.java
+++ src/Modules/PlayableThings/House.java
@@ -8,8 +8,47 @@ import java.util.ArrayList;
 public class House {
     private Card card;
     private Item item;
-    private ArrayList<Integer> numberOfFireTurn = new ArrayList();
-    private ArrayList<Integer> numberOfPoisonTurn = new ArrayList();
-    private ArrayList<Integer> numberOfHolyTurn = new ArrayList();
+    private ArrayList<Integer> fireTurns = new ArrayList<>();
+    private ArrayList<Integer> poisonTurns = new ArrayList();
+    private ArrayList<Integer> holyTurns = new ArrayList();
 
+    public String getCardId() {
+        return card.getId();
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void removeCard() {
+        card = null;
+    }
+
+    public void removeItem() {
+        item = null;
+    }
+
+    public void addFireBuff(int numberOfFireTurn) {
+        fireTurns.add(numberOfFireTurn);
+    }
+
+    public void addPoisonBuff(int numberOfPoisonTurn) {
+        poisonTurns.add(numberOfPoisonTurn);
+    }
+
+    public void addHolyBuff(int numberOfHolyTurn) {
+        holyTurns.add(numberOfHolyTurn);
+    }
 }
