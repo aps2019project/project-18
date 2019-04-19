524c424085ee245a870d1d9f9a72774fdc93cc66
invoker <omidrahmati2000@gmail.com>
2019-04-19 10:08:58 +0430

    create class House and add some field

---
+++ src/Modules/PlayableThings/House.java
@@ -0,0 +1,15 @@
+package Modules.PlayableThings;
+
+import Modules.PlayableThings.Item.Item;
+import Modules.PlayableThings.cards.Card;
+
+import java.util.ArrayList;
+
+public class House {
+    private Card card;
+    private Item item;
+    private ArrayList<Integer> numberOfFireTurn = new ArrayList();
+    private ArrayList<Integer> numberOfPoisonTurn = new ArrayList();
+    private ArrayList<Integer> numberOfHolyTurn = new ArrayList();
+
+}
