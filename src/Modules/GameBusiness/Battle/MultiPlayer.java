aeb2db0dde9737aef73321aa779aaf8888ebf7f5
invoker <omidrahmati2000@gmail.com>
2019-04-20 22:31:23 +0430

    add methods to class MultiPlayer

--- src/Modules/GameBusiness/Battle/MultiPlayer.java
+++ src/Modules/GameBusiness/Battle/MultiPlayer.java
@@ -1,11 +1,41 @@
 package Modules.GameBusiness.Battle;
 
 import Modules.Account;
+import Modules.Main;
+import View.View.ShowBattle;
 
 public class MultiPlayer {
-    public static void doOrder(){
+    public static void doOrder() {
         new Account().showLeaderboard();
+        showMenu();
+        while (true) {
+            String[] order = Main.scanner.nextLine().trim().split(" ");
+            if (order[0].equalsIgnoreCase("select") && order[1].equalsIgnoreCase("user")
+                    && order.length > 2) {
+//todo
+            } else if (order[0].equalsIgnoreCase("back") && order.length == 1) {
+                ShowBattle.showMenu();
+                return;
+            } else if (order[0].equalsIgnoreCase("help") && order.length == 1) {
+                showHelpMenu();
+            } else {
+                showInvalidCommand();
+            }
+        }
     }
 
+    public static void showMenu() {
+        System.out.println("1. Select user [user name]");
+        System.out.println("2. Back");
+        System.out.println("3. Help");
+    }
 
+    public static void showHelpMenu() {
+        System.out.println("1. Select user [user name] : select enemy user name");
+        System.out.println("2. Back : go to previous menu");
+    }
+
+    public static void showInvalidCommand() {
+        System.out.println("invalid command please use help");
+    }
 }
