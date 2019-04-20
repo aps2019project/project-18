2511b4e9adcf59fb3dc23536bcc8d5b7b64ec073
mfary <moha1379>
2019-04-20 13:25:05 +0430

    write cases in switch of playerController

--- src/Controller/PlayerController.java
+++ src/Controller/PlayerController.java
@@ -9,12 +9,37 @@ public class PlayerController {
     public void hadlePlayerCommands(Human haman){
         this.human = human;
         PlayerRequest request = new PlayerRequest();
-        request.getNewCommand();
 
         while (true){
-//            if (!request.isValid()){
-//                continue;
-//            }
+            request.getNewCommand();
+            if (!request.isValid()){
+                continue;
+            }
+            switch (request.getType()){
+                case HELP:
+
+                case SELECT:
+
+                case END_TURN:
+
+                case GAME_INFO:
+
+                case SHOW_HAND:
+
+                case INSERT_CARD:
+
+                case SHOW_CARD_INFO:
+
+                case SHOW_NEXT_CARD:
+
+                case ENTER_GRAVEYRAD:
+
+                case SHOW_MY_MINIONS:
+
+                case SHOW_COLLECTABLES:
+
+                case SHOW_OPPONENT_MINIONS:
+            }
         }
     }
 }
