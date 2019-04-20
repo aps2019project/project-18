9281f719d0a9b33bb7e9ef156ca60a447eefcfae
mfary <moha1379>
2019-04-20 15:04:28 +0430

    build getType method

---
+++ src/View/Request/Player/Graveyard/GraveyardRequest.java
@@ -0,0 +1,23 @@
+package View.Request.Player.Graveyard;
+
+import View.Request.MainRequest;
+
+public class GraveyardRequest extends MainRequest {
+    private static final String EXIT = "Exit";
+    private static final String SHOW_CARD = "Show info";
+    private static final String SHOW_CARDS = "Show cards";
+
+    public GraveyardRequestType getType() {
+        if (command == null || command.equals("")) {
+            return null;
+        }
+        if (command.equals(EXIT)) {
+            return GraveyardRequestType.EXIT;
+        } else if (command.substring(0, 8).equals(SHOW_CARD)) {
+            return GraveyardRequestType.SHOW_CARD;
+        } else if (command.equals(SHOW_CARDS)) {
+            return GraveyardRequestType.SHOW_CARDS;
+        }
+        return null;
+    }
+}
---
+++ src/View/Request/Player/Graveyard/GraveyardRequestType.java
@@ -0,0 +1,7 @@
+package View.Request.Player.Graveyard;
+
+public enum GraveyardRequestType {
+    EXIT,
+    SHOW_CARDS,
+    SHOW_CARD
+}
