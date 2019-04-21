7a38300d2492a96fcd94d398edc4f75e8847ac7b
invoker <omidrahmati2000@gmail.com>
2019-04-21 11:20:18 +0430

    add constructor to class ModeCaptureHalfFlags for set number of flags

--- src/Modules/GameBusiness/Game/ModeCaptureHalfFlags.java
+++ src/Modules/GameBusiness/Game/ModeCaptureHalfFlags.java
@@ -3,15 +3,25 @@ package Modules.GameBusiness.Game;
 import Modules.GameBusiness.Player.AI;
 import Modules.GameBusiness.Player.Human;
 
-import java.util.Random;
-
 public class ModeCaptureHalfFlags extends Game {
+    private int numberOfFlags = 7;
+
     public ModeCaptureHalfFlags(Human playerOne, Human playerTwo) {
         super(playerOne, playerTwo);
     }
 
     public ModeCaptureHalfFlags(Human playerOne, AI playerTwo) {
         super(playerOne, playerTwo);
+    }
+
+    public ModeCaptureHalfFlags(Human playerOne, Human playerTwo, int numberOfFlags) {
+        super(playerOne, playerTwo);
+        this.numberOfFlags = numberOfFlags;
+    }
+
+    public ModeCaptureHalfFlags(Human playerOne, AI playerTwo, int numberOfFlags) {
+        super(playerOne, playerTwo);
+        this.numberOfFlags = numberOfFlags;
     }
 
     @Override
