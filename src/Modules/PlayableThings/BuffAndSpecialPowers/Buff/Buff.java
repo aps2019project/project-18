d4c4f5ccf8a2f43a9a6197de842c92cfc3645066
mfary <moha1379>
2019-04-29 21:58:32 +0430

    handle buff but there is a problem with multi ones and  target

--- src/Modules/PlayableThings/BuffAndSpecialPowers/Buff/Buff.java
+++ src/Modules/PlayableThings/BuffAndSpecialPowers/Buff/Buff.java
@@ -1,15 +1,29 @@
 package Modules.PlayableThings.BuffAndSpecialPowers.Buff;
 
+import Modules.PlayableThings.cards.Force;
+
 public abstract class Buff {
-    private boolean positivity;
     private int numberOfTurns;
+    private int executeTime;
     private int[] effectPoint = new int[2];
     private int dimension;
     private Boolean disarm;
-    private Boolean
+    private int attackpower;
+    private int healthPower;
+    private Boolean fireHouse;
+    private Boolean poisonHouse;
+    private int hit;
+    private Boolean holy;
+    private Boolean kill;
+    private Boolean stun;
+    private boolean isContinious;
+    private Boolean deleteNegative;
+    private Boolean deletePositive;
+    private Force enemy;
+    private int turnsOfAttack;
+    //todo target
 
     public void doEffect(){
         //todo
     }
-
 }
