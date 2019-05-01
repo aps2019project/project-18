88e523841d28b079477144fec88b23f2939c2fab
mfary <moha1379>
2019-05-02 00:16:56 +0430

    same work for item

---
+++ src/View/Request/Player/Item/ItemRequest.java
@@ -0,0 +1,4 @@
+package View.Request.Player.Item;
+
+public class ItemRequest {
+}
---
+++ src/View/Request/Player/Item/ItemRequestType.java
@@ -0,0 +1,7 @@
+package View.Request.Player.Item;
+
+public enum ItemRequestType {
+    SHOW_INFO,
+    USE,
+    EXIT
+}
