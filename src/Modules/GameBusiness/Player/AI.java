b8223906f64655fc867ea19bd739851ce4c4ae86
invoker <omidrahmati2000@gmail.com>
2019-05-03 18:25:19 +0430

    set deck for Ai story mode

--- src/Modules/GameBusiness/Player/AI.java
+++ src/Modules/GameBusiness/Player/AI.java
@@ -1,4 +1,11 @@
 package Modules.GameBusiness.Player;
 
+import Modules.Deck;
+
 public class AI extends Player {
+    private Deck deck = new Deck("AI");
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
 }
