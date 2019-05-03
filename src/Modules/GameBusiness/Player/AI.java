ab59db98d9b5dc890408eac0077fe793df3f3bdd
mfary <moha1379>
2019-05-03 13:30:14 +0430

    AI

--- src/Modules/GameBusiness/Player/AI.java
+++ src/Modules/GameBusiness/Player/AI.java
@@ -1,4 +1,23 @@
 package Modules.GameBusiness.Player;
 
+
+import Modules.PlayableThings.cards.Card;
+
 public class AI extends Player {
+    Card[] putableCards;
+    int[] enemyHero = new int[2];
+
+    @Override
+    public void playTurn(int turn) {
+        super.playTurn(turn);
+        putableCards = hand.getPutableCards(manaPoint);
+        putCards();
+    }
+
+    private void putCards() {
+        for (Card card : putableCards){
+            int[][] placees= game.placesCanPut(this);
+        }
+    }
+
 }
