3a1ec6a6dd08e488c987d95c944def74c6238bef
AliLordLoss <a.a.ghanati@gmail.com>
2019-05-03 11:59:42 +0430

    added method showCard to class Graveyard

--- src/Modules/Graveyard.java
+++ src/Modules/Graveyard.java
@@ -1,6 +1,6 @@
 package Modules;
 
-import Modules.PlayableThings.cards.Card;
+import Modules.PlayableThings.cards.*;
 import View.View.Show;
 
 import java.util.ArrayList;
@@ -15,5 +15,17 @@ public class Graveyard {
     public void showGraveyard() {
         Show.get().showMinions(cards, null);
         Show.get().showSpells(cards, null);
+    }
+
+    public void showCard(String cardId) {
+        for (Card card : cards) {
+            if (card.getId().equals(cardId)) {
+                if (card instanceof Minion) {
+                    Show.get().showMinion((Minion) card);
+                } else {
+                    Show.get().showSpell((Spell) card);
+                }
+            }
+        }
     }
 }
