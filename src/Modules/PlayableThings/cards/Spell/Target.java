7d8126de3a9af96f31df97a807b9b83f4db0a41e
mfary <moha1379>
2019-05-05 00:01:04 +0430

    make class Target and delete target from buff and give it to spell

---
+++ src/Modules/PlayableThings/cards/Spell/Target.java
@@ -0,0 +1,10 @@
+package Modules.PlayableThings.cards.Spell;
+
+public class Target {
+    private boolean ground;
+    private int diminision;
+    private boolean all;
+    private boolean ally;
+    private int count;
+    private boolean hero;
+}
