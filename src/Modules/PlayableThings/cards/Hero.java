789f5030c9159db3b86d472f519924f7a2f9b977
mfary <moha1379>
2019-05-05 22:51:26 +0430

    add constructor fo hero and force and add special power to force

--- src/Modules/PlayableThings/cards/Hero.java
+++ src/Modules/PlayableThings/cards/Hero.java
@@ -1,5 +1,7 @@
 package Modules.PlayableThings.cards;
 
+import Modules.PlayableThings.BuffAndSpecialPowers.Buff.Buff;
+import Modules.PlayableThings.BuffAndSpecialPowers.SpecialPower.SpecialPower;
 import Modules.PlayableThings.cards.Spell.Spell;
 import View.View.Show;
 
@@ -9,10 +11,21 @@ public class Hero extends Force {
     private int usedSpellCoolDown = 0;
     private Spell spell;
 
-    public Hero(String name, String description, int price, int attackPower, int hitPoint, String attackType, int range, int spellCoolDown, int spellMana) {
+    public Hero(String name, String description, int price, int attackPower, int hitPoint, String attackType, int range, Spell spell , int spellCoolDown, int spellMana) {
         super(name, description, price, attackPower, hitPoint, attackType, range, 0);
+        this.spell = spell;
         this.spellCoolDown = spellCoolDown;
         this.spellMana = spellMana;
+    }
+
+    public Hero(String name, String description, int price, int attackPower, int hitPoint, String attackType, int range, int manaPoint , SpecialPower specialPower) {
+        super(name, description, price, attackPower, hitPoint, attackType, range, manaPoint , specialPower);
+        spell = null;
+    }
+
+    public Hero(String name, String description, int price, int attackPower, int hitPoint, String attackType, int range, int manaPoint , Buff buff) {
+        super( name, description, price, attackPower, hitPoint, attackType, range, manaPoint , buff);
+        spell = null;
     }
 
     public void excuteAbility() {
