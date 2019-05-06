dc2db61e4ed4a7d5a32eb6c38f31543e62589231
AliLordLoss <a.a.ghanati@gmail.com>
2019-05-06 14:23:26 +0430

    fixed item in class House & handled its use in class Game

--- src/Modules/GameBusiness/Game/Game.java
+++ src/Modules/GameBusiness/Game/Game.java
@@ -82,7 +82,7 @@ public abstract class Game {
         playground.getGround()[getPosition(force)[0]][getPosition(force)[1]].removeCard();
     }
 
-    public Item move(Force force, int x, int y) {
+    public Item[] move(Force force, int x, int y) {
         if (playground.getGround()[x - 1][y - 1].getCard() != null) {
             System.out.println("destination house is full");
             return null;
@@ -103,7 +103,9 @@ public abstract class Game {
             playground.move(getPosition(force)[0], getPosition(force)[1], x - 1, y - 1);
             force.moved();
             System.out.println("card moved");
-            return playground.getGround()[x - 1][y - 1].getItem();
+            Item[] items = playground.getGround()[x - 1][y - 1].getItem();
+            playground.getGround()[x - 1][y - 1].removeItems();
+            return items;
         }
     }
 
