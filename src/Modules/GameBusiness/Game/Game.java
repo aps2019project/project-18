e94f59cad03f127448d5a24fe5e0fef937af15cc
AliLordLoss <a.a.ghanati@gmail.com>
2019-05-06 09:53:23 +0430

    added method getMyCards in class game

--- src/Modules/GameBusiness/Game/Game.java
+++ src/Modules/GameBusiness/Game/Game.java
@@ -167,12 +167,23 @@ public abstract class Game {
     public Force getForce(String id) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 5; j++) {
-                if (playground.getGround()[i][j].getCard().getId().equals(id)) {
+                if (playground.getGround()[i][j].getCardId().equals(id)) {
                     return (Force) playground.getGround()[i][j].getCard();
                 }
             }
         }
         return null;
+    }
+
+    public Force[] getMyCards() {
+        ArrayList<Force> cards = new ArrayList<>();
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 5; j++) {
+                if (playground.getGround()[i][j] != null &&
+                        players[turn % 2].checkCard(playground.getGround()[i][j].getCardId()))
+                    cards.add((Force) playground.getGround()[i][j].getCard());
+            }
+        return (Force[]) cards.toArray();
     }
 
     public boolean insertCard(Card card, int x, int y) {
