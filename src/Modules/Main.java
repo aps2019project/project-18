4f3b2931017fc521f2f33e3fd331d78a2a36286d
AliLordLoss <a.a.ghanati@gmail.com>
2019-06-23 17:11:02 +0430

    ...

--- src/Modules/Main.java
+++ src/Modules/Main.java
@@ -5,6 +5,7 @@ import View.View.ShowMain;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -22,11 +23,31 @@ public class Main {
         Game.initializeItems();
     }
 
-    private static void signIn() {
-        ShowMain.showTextForSignIn();
-        String userName = scanner.nextLine();
-        String password = scanner.nextLine();
-        Account.signIn(userName, password);
+    private static void signIn(Scene scene) {
+        Group root = new Group();
+        scene = new Scene(root, 800, 800, Color.VIOLET);
+
+        TextField username = new TextField();
+        username.relocate(100, 200);
+        PasswordField password = new PasswordField();
+        password.relocate(100, 300);
+        Button apply = new Button("Sign In");
+        apply.relocate(100, 400);
+
+        Alert emptyFields = new Alert(Alert.AlertType.WARNING);
+        emptyFields.setTitle("Warning");
+        emptyFields.setHeaderText("Incorrect username/password");
+        emptyFields.setContentText("incorrect username or password, please check your entries");
+        apply.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                if (username.getText().equals("") || password.getText().equals("")) {
+
+                }
+            }
+        });
+
+        root.getChildren().addAll(username, password, apply);
+        Account.signIn(username, password);
     }
 
     private static void signUp(Scene scene) {
@@ -47,10 +68,9 @@ public class Main {
 
     public static void menu(Scene scene) {
         Group root = new Group();
-        scene = new Scene(root, Color.VIOLET);
+        scene = new Scene(root, 800, 800, Color.VIOLET);
 
-        Button signIn = new Button();
-        signIn.setText("Sign In");
+        Button signIn = new Button("Sign In");
         signIn.relocate(100, 100);
 
         Button exit = new Button("Exit");
@@ -67,6 +87,11 @@ public class Main {
                 signUp(scene);
         });
 
+        signIn.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                signIn(scene);
+            }
+        });
         root.getChildren().addAll(exit , signIn , signUp);
     }
 }
