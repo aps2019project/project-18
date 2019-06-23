17f0c374a631a8ab8d653508c7ce1afc95bd3d96
mfary <moha1379>
2019-06-23 16:17:14 +0430

    go to main menu

--- src/View/GraphicView.java
+++ src/View/GraphicView.java
@@ -1,11 +1,17 @@
 package View;
 
+import Modules.Main;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GraphicView extends Application {
+    private Scene scene;
     public void start(Stage primaryStage) {
-
+        Main.menu(scene);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setTitle("Duelist");
     }
 
     public static void main(String[] args) {
