79e6bc486695833cf7992b6e2992c5ad89b5d815
mfary <moha1379>
2019-06-23 15:44:12 +0430

    graphic class

---
+++ src/View/GraphicView.java
@@ -0,0 +1,14 @@
+package View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GraphicView extends Application {
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
