bab61e61dfe1cb2a8d014b1c73f52e81135d09bd
mfary <moha1379>
2019-06-23 16:59:30 +0430

    handle sign up

--- src/Modules/Main.java
+++ src/Modules/Main.java
@@ -2,9 +2,14 @@ package Modules;
 
 import Modules.GameBusiness.Game.Game;
 import View.View.ShowMain;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import java.util.Scanner;
@@ -24,10 +29,20 @@ public class Main {
         Account.signIn(userName, password);
     }
 
-    private static void signUp() {
-        ShowMain.showTextForInsertUserName();
-        String userName = scanner.nextLine();
-        Account.createAccount(userName);
+    private static void signUp(Scene scene) {
+        Group root = new Group();
+        scene = new Scene(root , 100 , 100 , Color.RED)
+        TextField userName = new TextField();
+        userName.relocate(0 , 20);
+        PasswordField password = new PasswordField();
+        password.relocate(50 , 20);
+        Button ok = new Button("Ok");
+        ok.relocate(50 ,100);
+        ok.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY)
+                Account.createAccount(userName.getText() , password.getText());
+        });
+        root.getChildren().addAll(ok , password , userName);
     }
 
     public static void menu(Scene scene) {
@@ -40,6 +55,18 @@ public class Main {
 
         Button exit = new Button("Exit");
         exit.relocate(100 , 500);
+        exit.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY)
+                System.exit(0);
+        });
 
+        Button signUp = new Button("Sign up");
+        signUp.relocate(100 , 300);
+        signUp.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY)
+                signUp(scene);
+        });
+
+        root.getChildren().addAll(exit , signIn , signUp);
     }
 }
