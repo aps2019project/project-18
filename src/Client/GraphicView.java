10b9d7e1b1bfcfd6aede86f8954d8a27ea38ceef
mfary <moha1379>
2019-07-07 10:49:53 +0430

    handle read and write

--- src/Client/GraphicView.java
+++ src/Client/GraphicView.java
@@ -8,11 +8,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class GraphicView extends Application {
     private Scene scene;
     private static Socket socket;
+    private static Scanner scanner;
+    private static Formatter formatter;
 
     public void start(Stage primaryStage) {
         Main.menu(primaryStage);
@@ -28,9 +31,13 @@ public class GraphicView extends Application {
             FileInputStream fileInputStream = new FileInputStream("C:\\Users\\asus\\Desktop\\project-18\\config.txt");
             Scanner scanner = new Scanner(fileInputStream);
             socket = new Socket(scanner.nextLine() , scanner.nextInt());
+            formatter = new Formatter(socket.getOutputStream());
+            scanner = new Scanner(socket.getInputStream());
         } catch (FileNotFoundException e) {
             try {
                 socket = new Socket("127.0.0.1" , 8000);
+                formatter = new Formatter(socket.getOutputStream());
+                scanner = new Scanner(socket.getInputStream());
             } catch (IOException e1) {
 
             }
@@ -39,5 +46,14 @@ public class GraphicView extends Application {
         } catch (IOException e) {
             System.out.println("server not found");
         }
+    }
+
+    synchronized public static void write(String line) {
+        formatter.format("%s\\n" , line);
+        formatter.flush();
+    }
+
+    synchronized public static String read() {
+        return scanner.nextLine()
     }
 }
