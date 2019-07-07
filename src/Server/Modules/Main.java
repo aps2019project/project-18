2dced0cf52f2e37e353e1bef4448b540a58bcfac
AliLordLoss <a.a.ghanati@gmail.com>
2019-07-07 11:56:52 +0430

    made a few changes in server side

--- src/Server/Modules/Main.java
+++ src/Server/Modules/Main.java
@@ -8,51 +8,68 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+        ServerSocket ss = null;
         while (true) {
             try {
                 FileInputStream fileInputStream = new FileInputStream("C:\\Users\\asus\\Desktop\\project-18\\src\\Server\\config.txt");
                 Scanner scanner = new Scanner(fileInputStream);
                 scanner.nextLine();
-                ServerSocket ss = new ServerSocket(scanner.nextInt());
-                Socket socket = ss.accept();
-                Formatter serverOutput = new Formatter(socket.getOutputStream());
-                Scanner serverScanner = new Scanner(socket.getInputStream());
-                if (serverScanner.nextLine().equals("sign up")) {
-                    String username = serverScanner.nextLine();
-                    if (!Account.checkExistUserName(username)) {
-                        Account.createAccount(username, serverScanner.nextLine());
+                ss = new ServerSocket(scanner.nextInt());
+            } catch (FileNotFoundException e) {
+                try {
+                    ss = new ServerSocket(8000);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Socket socket = null;
+            try {
+                socket = ss.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Formatter serverOutput = null;
+            try {
+                serverOutput = new Formatter(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scanner serverScanner = null;
+            try {
+                serverScanner = new Scanner(socket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (serverScanner.nextLine().equals("sign up")) {
+                String username = serverScanner.nextLine();
+                if (!Account.checkExistUserName(username)) {
+                    Account.createAccount(username, serverScanner.nextLine());
+                    //todo start account thread
+                    serverOutput.format("ok\n");
+                    serverOutput.flush();
+                } else {
+                    serverOutput.format("fuck\n");
+                    serverOutput.flush();
+                }
+            } else {
+                String username = serverScanner.nextLine();
+                if (Account.checkExistUserName(username)) {
+                    if (Account.findAccount(username).checkPassword(serverScanner.nextLine())) {
+                        Account.findAccount(username).signIn();
                         //todo start account thread
                         serverOutput.format("ok\n");
                         serverOutput.flush();
                     } else {
-                        serverOutput.format("fuck\n");
+                        serverOutput.format("pass\n");
                         serverOutput.flush();
                     }
                 } else {
-                    String username = serverScanner.nextLine();
-                    if (Account.checkExistUserName(username)) {
-                        if (Account.findAccount(username).checkPassword(serverScanner.nextLine())) {
-                            Account.findAccount(username).signIn();
-                            //todo start account thread
-                            serverOutput.format("ok\n");
-                            serverOutput.flush();
-                        } else {
-                            serverOutput.format("pass\n");
-                            serverOutput.flush();
-                        }
-                    } else {
-                        serverOutput.format("user\n");
-                        serverOutput.flush();
-                    }
+                    serverOutput.format("user\n");
+                    serverOutput.flush();
                 }
-            } catch (FileNotFoundException e) {
-                try {
-                    ServerSocket ss = new ServerSocket(8000);
-                } catch (IOException e1) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
